9d0fb14c55228ffbcebb2d492b3f78037f94e364
Ignacio Manríquez <i.manriquez01@ufromail.cl>
2024-05-21 14:05:05 -0400

    Commit v1.0
    Vue update

---
+++ marketplace-vue/src/pages/PrimeraVistaLibreria.vue
@@ -0,0 +1,321 @@
+<template>
+    <div class="m-primera-vista-de">
+      <div class="div-2">
+        <div class="overlap-group">
+          <div class="text-wrapper-6">Buscar Producto</div>
+        </div>
+        <Component class="component-2-instance" />
+        <div class="rectangle" />
+        <div class="overlap">
+          <div class="text-wrapper-7">Agregar productos</div>
+        </div>
+        <div class="overlap-2">
+          <div class="text-wrapper-8">Quitar productos</div>
+        </div>
+        <div class="overlap-3">
+          <div class="ellipse" />
+          <div class="ellipse-2" />
+          <div class="ellipse-3" />
+        </div>
+        <Component56 class="component-56-instance" image="image-18-12.png" />
+        <Component58 class="component-58-instance" image="image-18-13.png" />
+        <Component59 class="component-59-instance" image="image-18-14.png" />
+        <Component60 class="component-60-instance" image="image-18-15.png" />
+        <div class="overlap-4">
+          <Component56 class="component-21" image="image-18-16.png" />
+          <Component62 class="component-21" image="image-18-17.png" />
+        </div>
+        <Component63 class="component-63-instance" image="image-18-18.png" />
+        <Component64 class="component-64-instance" image="image-18-19.png" />
+        <Component65 class="component-65-instance" image="image-18-20.png" />
+        <div class="overlap-5">
+          <Component56 class="component-21" image="image-18-21.png" />
+          <Component67 class="component-21" image="image-18-22.png" />
+        </div>
+        <Component68 class="component-68-instance" image="image-18-23.png" />
+        <Component69 class="component-69-instance" image="image-18-24.png" />
+        <Component70 class="component-70-instance" image="image-18-25.png" />
+        <img class="pappel-copia" alt="Pappel copia" src="pappel-copia-removebg-preview-3.png" />
+      </div>
+    </div>
+</template>
+  
+<script>
+  import { Component } from "./Component";
+  import { Component56 } from "./Component56";
+  import { Component58 } from "./Component58";
+  import { Component59 } from "./Component59";
+  import { Component60 } from "./Component60";
+  import { Component62 } from "./Component62";
+  import { Component63 } from "./Component63";
+  import { Component64 } from "./Component64";
+  import { Component65 } from "./Component65";
+  import { Component67 } from "./Component67";
+  import { Component68 } from "./Component68";
+  import { Component69 } from "./Component69";
+  import { Component70 } from "./Component70";
+  
+  export default {
+    name: "MPrimeraVistaDe",
+    components: {
+      Component,
+      Component56,
+      Component58,
+      Component59,
+      Component60,
+      Component62,
+      Component63,
+      Component64,
+      Component65,
+      Component67,
+      Component68,
+      Component69,
+      Component70,
+    },
+  };
+</script>
+  
+<style>
+  .m-primera-vista-de {
+    background-color: #f1ecd0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+  }
+  
+  .m-primera-vista-de .div-2 {
+    background-color: #f1ecd0;
+    height: 1024px;
+    position: relative;
+    width: 1440px;
+  }
+  
+  .m-primera-vista-de .overlap-group {
+    background-image: url(./rectangle-2.svg);
+    background-size: 100% 100%;
+    height: 62px;
+    left: 289px;
+    position: absolute;
+    top: 47px;
+    width: 906px;
+  }
+  
+  .m-primera-vista-de .text-wrapper-6 {
+    color: #000000;
+    font-family: "Inter-Medium", Helvetica;
+    font-size: 20px;
+    font-weight: 500;
+    height: 34px;
+    left: 25px;
+    letter-spacing: 0;
+    line-height: normal;
+    opacity: 0.5;
+    position: absolute;
+    top: 13px;
+    width: 284px;
+  }
+  
+  .m-primera-vista-de .component-2-instance {
+    background-color: #f0dbaf !important;
+    left: 27px !important;
+    position: absolute !important;
+    top: 24px !important;
+  }
+  
+  .m-primera-vista-de .rectangle {
+    background-color: #f0dbaf;
+    box-shadow: 0px 4px 4px #00000040;
+    height: 69px;
+    left: 1321px;
+    position: absolute;
+    top: 24px;
+    width: 79px;
+  }
+  
+  .m-primera-vista-de .overlap {
+    background-color: #38c115;
+    border: 1px solid;
+    border-color: #000000;
+    border-radius: 40px;
+    height: 49px;
+    left: 202px;
+    position: absolute;
+    top: 139px;
+    width: 334px;
+  }
+  
+  .m-primera-vista-de .text-wrapper-7 {
+    color: #ffffff;
+    font-family: "Inter-Bold", Helvetica;
+    font-size: 30px;
+    font-weight: 700;
+    height: 36px;
+    left: 27px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 4px;
+    white-space: nowrap;
+  }
+  
+  .m-primera-vista-de .overlap-2 {
+    background-color: #fc0202;
+    border: 1px solid;
+    border-color: #000000;
+    border-radius: 40px;
+    height: 49px;
+    left: 861px;
+    position: absolute;
+    top: 139px;
+    width: 334px;
+  }
+  
+  .m-primera-vista-de .text-wrapper-8 {
+    color: #ffffff;
+    font-family: "Inter-Bold", Helvetica;
+    font-size: 30px;
+    font-weight: 700;
+    height: 36px;
+    left: 41px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 4px;
+    white-space: nowrap;
+  }
+  
+  .m-primera-vista-de .overlap-3 {
+    background-color: #f0dbaf;
+    border: 1px solid;
+    border-color: #000000;
+    height: 826px;
+    left: 1376px;
+    position: absolute;
+    top: 162px;
+    width: 48px;
+  }
+  
+  .m-primera-vista-de .ellipse {
+    background-color: #ffffff;
+    border-radius: 7.5px;
+    height: 15px;
+    left: 18px;
+    position: absolute;
+    top: 430px;
+    width: 15px;
+  }
+  
+  .m-primera-vista-de .ellipse-2 {
+    background-color: #ffffff;
+    border-radius: 7.5px;
+    height: 15px;
+    left: 18px;
+    position: absolute;
+    top: 380px;
+    width: 15px;
+  }
+  
+  .m-primera-vista-de .ellipse-3 {
+    background-color: #ffffff;
+    border-radius: 7.5px;
+    height: 15px;
+    left: 18px;
+    position: absolute;
+    top: 330px;
+    width: 15px;
+  }
+  
+  .m-primera-vista-de .component-56-instance {
+    left: 73px !important;
+    position: absolute !important;
+    top: 219px !important;
+  }
+  
+  .m-primera-vista-de .component-58-instance {
+    left: 406px !important;
+    position: absolute !important;
+    top: 219px !important;
+  }
+  
+  .m-primera-vista-de .component-59-instance {
+    left: 739px !important;
+    position: absolute !important;
+    top: 219px !important;
+  }
+  
+  .m-primera-vista-de .component-60-instance {
+    left: 1072px !important;
+    position: absolute !important;
+    top: 219px !important;
+  }
+  
+  .m-primera-vista-de .overlap-4 {
+    border-radius: 10px;
+    height: 199px;
+    left: 73px;
+    position: absolute;
+    top: 503px;
+    width: 250px;
+  }
+  
+  .m-primera-vista-de .component-21 {
+    left: 0 !important;
+    position: absolute !important;
+    top: 0 !important;
+  }
+  
+  .m-primera-vista-de .component-63-instance {
+    left: 406px !important;
+    position: absolute !important;
+    top: 503px !important;
+  }
+  
+  .m-primera-vista-de .component-64-instance {
+    left: 739px !important;
+    position: absolute !important;
+    top: 503px !important;
+  }
+  
+  .m-primera-vista-de .component-65-instance {
+    left: 1072px !important;
+    position: absolute !important;
+    top: 503px !important;
+  }
+  
+  .m-primera-vista-de .overlap-5 {
+    border-radius: 10px;
+    height: 199px;
+    left: 73px;
+    position: absolute;
+    top: 787px;
+    width: 250px;
+  }
+  
+  .m-primera-vista-de .component-68-instance {
+    left: 406px !important;
+    position: absolute !important;
+    top: 787px !important;
+  }
+  
+  .m-primera-vista-de .component-69-instance {
+    left: 739px !important;
+    position: absolute !important;
+    top: 787px !important;
+  }
+  
+  .m-primera-vista-de .component-70-instance {
+    left: 1072px !important;
+    position: absolute !important;
+    top: 787px !important;
+  }
+  
+  .m-primera-vista-de .pappel-copia {
+    height: 98px;
+    left: 139px;
+    position: absolute;
+    top: 24px;
+    width: 100px;
+  }
+</style>
+  
---
+++ marketplace-vue/src/pages/RegistroLibreria.vue
@@ -0,0 +1,466 @@
+<template>
+    <div class="m-registro-usuario">
+      <div class="div">
+        <div class="overlap-group">
+          <Component8 class="component" color="#6C4527" />
+          <div class="overlap">
+            <div class="text-wrapper">URL</div>
+          </div>
+          <div class="overlap-2">
+            <div class="rectangle" />
+            <div class="text-wrapper-2">Correo</div>
+          </div>
+          <div class="div-wrapper">
+            <div class="text-wrapper-3">Contraseña</div>
+          </div>
+          <div class="overlap-3">
+            <div class="rectangle-2" />
+            <div class="text-wrapper-4">Repetir Contraseña</div>
+          </div>
+          <div class="overlap-4">
+            <div class="rectangle-3" />
+            <div class="text-wrapper-5">Registrarse</div>
+          </div>
+          <div class="overlap-5">
+            <div class="rectangle-4" />
+            <div class="text-wrapper-6">Nombre empresa</div>
+          </div>
+          <div class="overlap-6">
+            <div class="text-wrapper-7">Inicio de cuenta</div>
+          </div>
+          <div class="overlap-7">
+            <div class="text-wrapper-8">Registrar cuenta</div>
+          </div>
+          <div class="overlap-8">
+            <div class="text-wrapper-9">Usuario Comprador</div>
+          </div>
+          <div class="overlap-9">
+            <div class="text-wrapper-10">Usuario Libreria</div>
+          </div>
+          <div class="overlap-10">
+            <div class="rectangle-5" />
+            <div class="text-wrapper-11">Colorful Pencils</div>
+          </div>
+          <div class="overlap-11">
+            <div class="rectangle-5" />
+            <div class="text-wrapper-11">contact.colorfulpencilscl@gmail.com</div>
+          </div>
+          <div class="overlap-12">
+            <div class="rectangle-5" />
+            <div class="text-wrapper-11">****************</div>
+          </div>
+          <div class="overlap-13">
+            <div class="rectangle-5" />
+            <div class="text-wrapper-11">****************</div>
+          </div>
+          <div class="overlap-14">
+            <div class="rectangle-5" />
+            <div class="text-wrapper-11">www.colorfulpencilschile.cl</div>
+          </div>
+        </div>
+        <img class="pappel-copia" alt="Pappel copia" src="pappel-copia-removebg-preview-2.png" />
+      </div>
+    </div>
+</template>
+  
+<script>
+  import { Component8 } from "./Component8";
+  
+  export default {
+    name: "MRegistroUsuario",
+    components: {
+      Component8,
+    },
+  };
+</script>
+  
+<style>
+  .m-registro-usuario {
+    background-color: #f1ecd0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    width: 100%;
+  }
+  
+  .m-registro-usuario .div {
+    background-color: #f1ecd0;
+    height: 1024px;
+    position: relative;
+    width: 1440px;
+  }
+  
+  .m-registro-usuario .overlap-group {
+    background-color: #e5d6a3;
+    height: 810px;
+    left: 107px;
+    position: absolute;
+    top: 152px;
+    width: 1225px;
+  }
+  
+  .m-registro-usuario .component {
+    height: 40px !important;
+    left: 18px !important;
+    position: absolute !important;
+    top: 16px !important;
+    width: 40px !important;
+  }
+  
+  .m-registro-usuario .overlap {
+    background-color: #6c4527;
+    border-radius: 30px;
+    height: 51px;
+    left: 60px;
+    position: absolute;
+    top: 585px;
+    width: 288px;
+  }
+  
+  .m-registro-usuario .text-wrapper {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 45px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 5px;
+    width: 184px;
+  }
+  
+  .m-registro-usuario .overlap-2 {
+    height: 51px;
+    left: 632px;
+    position: absolute;
+    top: 267px;
+    width: 291px;
+  }
+  
+  .m-registro-usuario .rectangle {
+    background-color: #6c4527;
+    border-radius: 30px;
+    height: 51px;
+    left: 25px;
+    position: absolute;
+    top: 0;
+    width: 241px;
+  }
+  
+  .m-registro-usuario .text-wrapper-2 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 4px;
+    width: 291px;
+  }
+  
+  .m-registro-usuario .div-wrapper {
+    background-color: #6c4527;
+    border-radius: 30px;
+    height: 51px;
+    left: 60px;
+    position: absolute;
+    top: 429px;
+    width: 290px;
+  }
+  
+  .m-registro-usuario .text-wrapper-3 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 52px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 6px;
+    width: 184px;
+  }
+  
+  .m-registro-usuario .overlap-3 {
+    height: 51px;
+    left: 655px;
+    position: absolute;
+    top: 427px;
+    width: 295px;
+  }
+  
+  .m-registro-usuario .rectangle-2 {
+    background-color: #6c4527;
+    border-radius: 30px;
+    height: 51px;
+    left: 2px;
+    position: absolute;
+    top: 0;
+    width: 290px;
+  }
+  
+  .m-registro-usuario .text-wrapper-4 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 30px;
+    font-weight: 400;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 7px;
+    width: 295px;
+  }
+  
+  .m-registro-usuario .overlap-4 {
+    border-radius: 10px;
+    height: 60px;
+    left: 371px;
+    position: absolute;
+    top: 731px;
+    width: 483px;
+  }
+  
+  .m-registro-usuario .rectangle-3 {
+    background-color: #6c4527;
+    border: 1px solid;
+    border-color: #000000;
+    border-radius: 10px;
+    height: 60px;
+    left: 0;
+    position: absolute;
+    top: 0;
+    width: 483px;
+  }
+  
+  .m-registro-usuario .text-wrapper-5 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 150px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 10px;
+    width: 184px;
+  }
+  
+  .m-registro-usuario .overlap-5 {
+    height: 51px;
+    left: 57px;
+    position: absolute;
+    top: 270px;
+    width: 291px;
+  }
+  
+  .m-registro-usuario .rectangle-4 {
+    background-color: #6c4527;
+    border-radius: 30px;
+    height: 51px;
+    left: 3px;
+    position: absolute;
+    top: 0;
+    width: 288px;
+  }
+  
+  .m-registro-usuario .text-wrapper-6 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 32px;
+    font-weight: 400;
+    left: 0;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    text-align: center;
+    top: 6px;
+    width: 291px;
+  }
+  
+  .m-registro-usuario .overlap-6 {
+    background-color: #6c4527;
+    border-radius: 5px;
+    height: 80px;
+    left: 339px;
+    position: absolute;
+    top: 30px;
+    width: 250px;
+  }
+  
+  .m-registro-usuario .text-wrapper-7 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 30px;
+    font-weight: 400;
+    left: 11px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 21px;
+    white-space: nowrap;
+  }
+  
+  .m-registro-usuario .overlap-7 {
+    background-color: #6c4527;
+    border-radius: 5px;
+    height: 80px;
+    left: 637px;
+    position: absolute;
+    top: 30px;
+    width: 250px;
+  }
+  
+  .m-registro-usuario .text-wrapper-8 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 30px;
+    font-weight: 400;
+    left: 9px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 22px;
+    white-space: nowrap;
+  }
+  
+  .m-registro-usuario .overlap-8 {
+    background-color: #6c4527;
+    border-radius: 5px;
+    height: 80px;
+    left: 213px;
+    position: absolute;
+    top: 149px;
+    width: 365px;
+  }
+  
+  .m-registro-usuario .text-wrapper-9 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 30px;
+    font-weight: 400;
+    left: 40px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 21px;
+    white-space: nowrap;
+  }
+  
+  .m-registro-usuario .overlap-9 {
+    background-color: #6c4527;
+    border: 1px solid;
+    border-color: #000000;
+    border-radius: 5px;
+    height: 80px;
+    left: 648px;
+    position: absolute;
+    top: 149px;
+    width: 365px;
+  }
+  
+  .m-registro-usuario .text-wrapper-10 {
+    color: #ffffff;
+    font-family: "Inter-Regular", Helvetica;
+    font-size: 30px;
+    font-weight: 400;
+    left: 70px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 22px;
+    white-space: nowrap;
+  }
+  
+  .m-registro-usuario .overlap-10 {
+    background-color: #6c4527;
+    border-radius: 10px;
+    height: 52px;
+    left: 60px;
+    position: absolute;
+    top: 332px;
+    width: 519px;
+  }
+  
+  .m-registro-usuario .rectangle-5 {
+    background-color: #eaeaea;
+    border-radius: 10px;
+    height: 41px;
+    left: 9px;
+    position: absolute;
+    top: 6px;
+    width: 501px;
+  }
+  
+  .m-registro-usuario .text-wrapper-11 {
+    color: #00000080;
+    font-family: "Inter-Medium", Helvetica;
+    font-size: 22px;
+    font-weight: 500;
+    height: 31px;
+    left: 18px;
+    letter-spacing: 0;
+    line-height: normal;
+    position: absolute;
+    top: 8px;
+    width: 449px;
+  }
+  
+  .m-registro-usuario .overlap-11 {
+    background-color: #6c4527;
+    border-radius: 10px;
+    height: 52px;
+    left: 657px;
+    position: absolute;
+    top: 332px;
+    width: 519px;
+  }
+  
+  .m-registro-usuario .overlap-12 {
+    background-color: #6c4527;
+    border-radius: 10px;
+    height: 52px;
+    left: 657px;
+    position: absolute;
+    top: 490px;
+    width: 519px;
+  }
+  
+  .m-registro-usuario .overlap-13 {
+    background-color: #6c4527;
+    border-radius: 10px;
+    height: 52px;
+    left: 60px;
+    position: absolute;
+    top: 492px;
+    width: 519px;
+  }
+  
+  .m-registro-usuario .overlap-14 {
+    background-color: #6c4527;
+    border-radius: 10px;
+    height: 52px;
+    left: 60px;
+    position: absolute;
+    top: 648px;
+    width: 519px;
+  }
+  
+  .m-registro-usuario .pappel-copia {
+    height: 98px;
+    left: 57px;
+    position: absolute;
+    top: 19px;
+    width: 100px;
+  }
+  
+</style>
+  
